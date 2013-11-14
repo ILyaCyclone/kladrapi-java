c2a34fc4b493d9940e4ede1054711641f8f6b3a6
ILyaCyclone <ilya.cyclone@gmail.com>
2013-11-14 17:55:08 +0400

    Обновлён пример использования

--- KladrApiClient/KladrApiClient/tests/cyclone/tools/kladrapi/client/SimpleTest.java
+++ KladrApiClient/KladrApiClient/tests/cyclone/tools/kladrapi/client/SimpleTest.java
@@ -1,21 +1,12 @@
 package cyclone.tools.kladrapi.client;
 
-import cyclone.tools.kladrapi.client.KladrApiClient;
-import cyclone.tools.kladrapi.client.KladrApiUrlBuilder;
-import cyclone.tools.kladrapi.client.KladrObject;
 
 import java.io.IOException;
-
 import java.io.PrintStream;
 
 import java.util.List;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Handler;
-import java.util.logging.Level;
 import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
-import org.json.JSONArray;
 
 public class SimpleTest {
     public SimpleTest() {
@@ -28,46 +19,66 @@ public class SimpleTest {
         } catch (IOException e) {
             System.err.println("Could not setup logger configuration: " + e.toString());
         }
+        
+        // демонстрационные токен и ключ
+        String token = "51dfe5d42fb2b43e3300006e";
+        String key = "86a2c2a06f1b2451a87d05512cc2c3edfdf41969";
 
-        KladrApiClient client = new KladrApiClient(KladrApiClientTest.DEMO_TOKEN, KladrApiClientTest.DEMO_KEY);
-        client.setProxy(null, null);
+        // создаём экземпляр клиента API с токеном и ключом
+        KladrApiClient client = new KladrApiClient(token, key);
 
-        List<KladrObject> cityList = client.getKladrCities("мос", 3, true);
-        System.out.println("cityList = " + cityList);
-        for (KladrObject ko : cityList) {
-            printKladrObject(ko, System.out);
+        // запрашиваем названия населённых пунктов на "мос", максимум 5 результатов
+        List<String> cityNameList = client.getCityNames("мос", 5);
+        System.out.println("Города на \"мос\" = " + cityNameList);
+        System.out.println();
+
+        // запрашиваем КЛАДР-объекты улиц, названия которых начинаются на "ново", в городе "москва"
+        // максимум 5 результатов
+        List<KladrObject> streetList = client.getKladrStreetsByCityName("ново", "москва", 5);
+        System.out.println("Улицы Москвы на \"ново\":");
+        for (KladrObject street : streetList) {
+            System.out.println(street.getType() + " " + street.getName());
         }
-        System.out.println("-----------------------");
 
-//        List<String> cityNameList = client.getCityNames("мос", 5);
-//        System.out.println("cityNameList = " + cityNameList);
-//        System.out.println("-----------------------");
-//
-//        List<KladrObject> streetList = client.getKladrStreetsByCityId("ново", "7700000000000", 3);
-//        System.out.println("streetList = " + streetList);
-//        for (KladrObject ko : streetList) {
-//            printKladrObject(ko, System.out);
-//        }
-//        System.out.println("-----------------------");
-//
-//        List<KladrObject> streetListByName = client.getKladrStreetsByCityName("пав", "мос", 10);
-//        System.out.println("streetListByName = " + streetListByName);
-//        System.out.println("-----------------------");
-//
-//
-//        List<KladrObject> districtList = client.getKladrDistricts("арх", 3);
-//        System.out.println("districtList = " + districtList);
-//        for (KladrObject district : districtList) {
-//            printKladrObject(district, System.out);
-//        }
-//
-//        List<String> districtNameList = client.getDistrictNames("арх", 3);
-//        System.out.println("districtNameList = " + districtNameList);
-//        System.out.println("-----------------------");
-//
-//        JSONArray jsonArray =
-//            client.getKladrJSONArray(new KladrApiUrlBuilder().setContentType(KladrApiUrlBuilder.CONTENT_TYPE_CITY).setLimit(3).setQuery("мос").toString());
-//        System.out.println(jsonArray);
+        //        KladrApiClient client = new KladrApiClient(KladrApiClientTest.DEMO_TOKEN, KladrApiClientTest.DEMO_KEY);
+        //        client.setProxy(null, null);
+        //
+        //        List<KladrObject> cityList = client.getKladrCities("мос", 3, true);
+        //        System.out.println("cityList = " + cityList);
+        //        for (KladrObject ko : cityList) {
+        //            printKladrObject(ko, System.out);
+        //        }
+        //        System.out.println("-----------------------");
+
+        //        List<String> cityNameList = client.getCityNames("мос", 5);
+        //        System.out.println("cityNameList = " + cityNameList);
+        //        System.out.println("-----------------------");
+        //
+        //        List<KladrObject> streetList = client.getKladrStreetsByCityId("ново", "7700000000000", 3);
+        //        System.out.println("streetList = " + streetList);
+        //        for (KladrObject ko : streetList) {
+        //            printKladrObject(ko, System.out);
+        //        }
+        //        System.out.println("-----------------------");
+        //
+        //        List<KladrObject> streetListByName = client.getKladrStreetsByCityName("пав", "мос", 10);
+        //        System.out.println("streetListByName = " + streetListByName);
+        //        System.out.println("-----------------------");
+        //
+        //
+        //        List<KladrObject> districtList = client.getKladrDistricts("арх", 3);
+        //        System.out.println("districtList = " + districtList);
+        //        for (KladrObject district : districtList) {
+        //            printKladrObject(district, System.out);
+        //        }
+        //
+        //        List<String> districtNameList = client.getDistrictNames("арх", 3);
+        //        System.out.println("districtNameList = " + districtNameList);
+        //        System.out.println("-----------------------");
+        //
+        //        JSONArray jsonArray =
+        //            client.getKladrJSONArray(new KladrApiUrlBuilder().setContentType(KladrApiUrlBuilder.CONTENT_TYPE_CITY).setLimit(3).setQuery("мос").toString());
+        //        System.out.println(jsonArray);
 
     }
 
--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-Java client for kladr-api.ru
+﻿Java client for kladr-api.ru
 
 Requires org.json JAR in you classpath.
 See: http://www.json.org/ , https://code.google.com/p/org-json-java/downloads/list 
@@ -8,16 +8,20 @@ Oracle JDeveloper 11.1.2.3 project.
 Usage Example:
 // демонстрационные токен и ключ
 String token = "51dfe5d42fb2b43e3300006e";
-String key = "86a2c2a06f1b2451a87d05512cc2c3edfdf41969"
+String key = "86a2c2a06f1b2451a87d05512cc2c3edfdf41969";
 
 // создаём экземпляр клиента API с токеном и ключом
 KladrApiClient client = new KladrApiClient(token, key);
 
 // запрашиваем названия населённых пунктов на "мос", максимум 5 результатов
 List<String> cityNameList = client.getCityNames("мос", 5);
-System.out.println("cityNameList = " + cityNameList);
+System.out.println("Города на \"мос\" = " + cityNameList);
+System.out.println();
 
 // запрашиваем КЛАДР-объекты улиц, названия которых начинаются на "ново", в городе "москва"
 // максимум 5 результатов
 List<KladrObject> streetList = client.getKladrStreetsByCityName("ново", "москва", 5);
-System.out.println("streetList = " + streetList);
+System.out.println("Улицы Москвы на \"ново\":");
+for (KladrObject street : streetList) {
+    System.out.println(street.getType() + " " + street.getName());
+}
